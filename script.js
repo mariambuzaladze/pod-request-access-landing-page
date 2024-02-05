3179faa3aec55c64affdba15cdae2e1c3ad928e8
mariambuzaladze <mariambuzaladze10@example.come>
2024-02-05 23:47:49 +0400

    add responsive design

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 let input = document.getElementsByClassName("b1")[0];
-console.log(input);
+let errorMessage = document.getElementsByClassName("error")[0];
 
 input.addEventListener("input", function () {
   let userInput = input.value.trim();
   let last = userInput.split("@");
 
   if (last[1] !== "mail.com") {
-    console.log("error");
+    errorMessage.style.display = "inline-block";
+  } else {
+    errorMessage.style.display = "none";
+  }
+
+  if (userInput.length === 0) {
+    errorMessage.style.display = "none";
   }
 });
